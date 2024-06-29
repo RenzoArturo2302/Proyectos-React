07ce3d4fdd786e615013aac7dd1c33e2c3f7c073
RenzoArturo2302 <renzoarturo12345@gmail.com>
2024-06-28 21:24:29 -0500

    Ayuda

--- 01-1-ProyectoG19/vite.config.js
+++ 01-1-ProyectoG19/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://primeforum-g19.vercel.app/",
+      "/api": "https://primeforum-g19.vercel.app",
     },
   },
   plugins: [react()],
